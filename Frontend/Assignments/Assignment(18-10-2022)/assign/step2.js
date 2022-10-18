a241c8e3cc36162e30abcbceee4f088fe9c80dea
b-moulidhar <65845778+b-moulidhar@users.noreply.github.com>
2022-10-18 22:21:53 +0530

    Update step2.js

--- Frontend/Assignments/Assignment(18-10-2022)/assign/step2.js
+++ Frontend/Assignments/Assignment(18-10-2022)/assign/step2.js
@@ -2,13 +2,39 @@ const redux = require("redux");
 let createStore = redux.legacy_createStore;
 let fs = require("fs")
 let process = require("process");
-let text = `firstname ${process.argv[2]} \n lastname ${process.argv[3]} \ncity ${process.argv[4]} \n`
-
+const { argv } = require("process");
+let obj;
+if(process.argv[2]){
+     obj = {
+        title: process.argv[2],
+        firstname: process.argv[3] || "no name given",
+        lastname: process.argv[4] || "no name given",      
+}
+}else{
+    console.log(obj = {
+        title: "no title given",
+        firstname: "no name given",
+        lastname: "no name given",      
+});
+console.log("please enter the title, firstname, lastname in the arguments")
+}
 const USERS_REQUEST = "USERS_REQUEST"
 
 let data = new Array();
+let usersjson = fs.readFileSync("data.json","utf-8");
+let users = JSON.parse(usersjson);
+users.push(obj);
+usersjson = JSON.stringify(users);
+fs.writeFileSync("data.json",usersjson,"utf-8");
 
-fs.appendFile("data.txt",text, 'utf-8',(err)=>{console.log("error",err)});
+
+// fs.appendFile("data.json",text, 'utf-8',(err)=>{ 
+//     if(err){
+//     console.log(err);
+//     }else {
+//     console.log("Contents of file appended");
+//     //   fs.readFileSync("../data/test.txt", "utf8"));
+//   }});
 
 let fetchUsers = ()=>{
     return {
@@ -17,19 +43,11 @@ let fetchUsers = ()=>{
     }
 }
 
-let initialState = {
-    // sl : process.argv[2],
-    // title : process.argv[3],
-    // gender: process.argv[4],
-    // firstname :process.argv[5],
-    // lastname : process.argv[6],
-    // city : process.argv[7]
-}
- let datas = fs.readFileSync("data.txt","utf-8")
+data.push( fs.readFileSync("data.txt","utf-8"))
 
 let reducer = (state,action)=>{
     switch(action.type){
-        case USERS_REQUEST : return datas;
+        case USERS_REQUEST : return users;
        
         default : return state
     }
@@ -40,5 +58,4 @@ let store = createStore(reducer);
 store.subscribe(()=>{
     console.log(store.getState());
 });
-
 store.dispatch(fetchUsers())
